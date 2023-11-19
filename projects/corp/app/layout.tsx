f6c003bac1625adcf81462e587ba0a1bb7de4353
LeeTrent <lee.trent.1@gmail.com>
2023-11-19 09:51:37 -0600

    Pleace nav links in layout.tsx

--- projects/corp/app/layout.tsx
+++ projects/corp/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Link from 'next/link';
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,7 +17,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Link href="/">Home</Link>
+        <Link href="/performance">Performance</Link>
+        <Link href="/reliablity">Reliablity</Link>
+        <Link href="/scale">Scale</Link>
+        {children}
+      </body>
     </html>
   )
 }
